06feef68eb379f2158bfb8455018dd2cc623b343
Janessa Garrow <janessa@apollographql.com>
2021-10-14 15:12:47 -0600

    Implement Trips component

--- src/pages/past-trips.js
+++ src/pages/past-trips.js
@@ -1,11 +1,31 @@
 import Layout from '../layouts/Layout';
 import React from 'react';
-import {Heading} from '@chakra-ui/react';
+import Trips from '../components/Trips';
+import {Center, Spinner} from '@chakra-ui/react';
+import {GUEST_TRIPS} from '../utils';
+import {useQuery} from '@apollo/client';
 
 export default function PastTrips() {
+  const {loading, error, data} = useQuery(GUEST_TRIPS);
+
+  if (loading) {
+    return (
+      <Center minH="100vh">
+        <Spinner size="lg" />
+      </Center>
+    );
+  }
+  if (error) {
+    return <div>uhoh error! {error.message}</div>;
+  }
+
+  const pastTrips = data.guestBookings.filter(
+    trip => trip.status === 'COMPLETED'
+  );
+
   return (
     <Layout>
-      <Heading as="h1">My Past trips</Heading>
+      <Trips trips={pastTrips} />
     </Layout>
   );
 }
--- src/pages/trips.js
+++ src/pages/trips.js
@@ -1,10 +1,31 @@
+import CurrentTrips from '../components/Trips';
 import Layout from '../layouts/Layout';
 import React from 'react';
-import {Heading} from '@chakra-ui/react';
+import {Center, Spinner} from '@chakra-ui/react';
+import {GUEST_TRIPS} from '../utils';
+import {useQuery} from '@apollo/client';
+
 export default function Trips() {
+  const {loading, error, data} = useQuery(GUEST_TRIPS);
+
+  if (loading) {
+    return (
+      <Center minH="100vh">
+        <Spinner size="lg" />
+      </Center>
+    );
+  }
+  if (error) {
+    return <div>uhoh error! {error.message}</div>;
+  }
+
+  const currentTrips = data.guestBookings.filter(
+    trip => trip.status !== 'COMPLETED'
+  );
+
   return (
     <Layout>
-      <Heading as="h1">My Trips</Heading>
+      <CurrentTrips trips={currentTrips} />
     </Layout>
   );
 }
