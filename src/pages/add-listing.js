08f0136abe7a360c1b2b765ea96e7dc71c8bfd73
Janessa Garrow <janessa@apollographql.com>
2021-10-25 11:35:47 -0600

    Add back button to add listings page

--- src/pages/add-listing.js
+++ src/pages/add-listing.js
@@ -1,6 +1,9 @@
 import Layout from '../layouts/Layout';
 import ListingForm from '../components/ListingForm';
 import React from 'react';
+import {Button} from '@chakra-ui/react';
+import {IoArrowBackOutline} from 'react-icons/io5';
+import {Link} from 'react-router-dom';
 import {gql} from '@apollo/client';
 
 export const CREATE_LISTING = gql`
@@ -29,6 +32,9 @@ export const CREATE_LISTING = gql`
 export default function CreateListing() {
   return (
     <Layout>
+      <Button as={Link} to="/listings" leftIcon={<IoArrowBackOutline />} mb="4">
+        Back to listings
+      </Button>
       <ListingForm mutation={CREATE_LISTING} />
     </Layout>
   );
