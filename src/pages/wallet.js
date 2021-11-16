a89e2d1af13e6871a9a339c25d71eda0fc41fb79
Janessa Garrow <janessa@apollographql.com>
2021-11-16 14:31:40 -0700

    Styling updates to wallet page

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -1,6 +1,18 @@
 import Layout from '../layouts/Layout';
-import React from 'react';
-import {Button, Heading, Stack, Text} from '@chakra-ui/react';
+import React, {useState} from 'react';
+import {
+  Button,
+  Heading,
+  InputGroup,
+  InputLeftAddon,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Stack,
+  Text
+} from '@chakra-ui/react';
 import {IoWallet} from 'react-icons/io5';
 import {gql, useMutation} from '@apollo/client';
 import {useUser} from '../utils';
@@ -17,6 +29,7 @@ export const ADD_FUNDS = gql`
 `;
 
 export default function Wallet() {
+  const [funds, setFunds] = useState(1);
   const {user, setUser} = useUser();
   const [addFundsToWallet] = useMutation(ADD_FUNDS, {
     onCompleted: data => {
@@ -26,22 +39,48 @@ export default function Wallet() {
 
   return (
     <Layout>
-      <Stack direction="row" spacing="4" align="center">
-        <Heading as="h1">My Wallet</Heading>
-        <IoWallet size="40" />
+      <Stack spacing="4">
+        <Stack direction="row" spacing="4" align="center">
+          <Heading as="h1">My Wallet</Heading>
+          <IoWallet size="40" />
+        </Stack>
+        {user && <Text>Current balance: ${user.funds}</Text>}
+        <InputGroup>
+          <InputLeftAddon bg="transparent" paddingRight="0">
+            $
+          </InputLeftAddon>
+          <NumberInput
+            name="numOfBeds"
+            min={1}
+            value={funds}
+            onChange={(_, val) => {
+              setFunds(val);
+            }}
+          >
+            <NumberInputField
+              borderLeftWidth="0"
+              borderTopLeftRadius="0"
+              borderBottomLeftRadius="0"
+            />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </InputGroup>
+        <Button
+          maxW="150px"
+          onClick={() =>
+            addFundsToWallet({
+              variables: {
+                amount: funds
+              }
+            })
+          }
+        >
+          Add Funds!
+        </Button>
       </Stack>
-      {user && <Text>Current balance: ${user.funds}</Text>}
-      <Button
-        onClick={() =>
-          addFundsToWallet({
-            variables: {
-              amount: 100
-            }
-          })
-        }
-      >
-        Add Funds!
-      </Button>
     </Layout>
   );
 }
