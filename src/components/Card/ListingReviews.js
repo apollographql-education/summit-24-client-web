3481c3db2dac3b70a9de03b031b47a4909a45e82
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-02-08 15:45:38 -0500

    fix: localStorage should use getItem method

--- src/components/Card/ListingReviews.js
+++ src/components/Card/ListingReviews.js
@@ -16,7 +16,7 @@ export function ListingReviews({isOpen, title, isPast, trip, mutationConfig}) {
         hostReview={hostReview}
         guestReview={guestReview}
         isPastTrip={isPast}
-        isHost={trip.listing.host.id === localStorage.id}
+        isHost={trip.listing.host.id === localStorage.getItem('token')}
         mutation={mutation}
         mutationOptions={mutationOptions}
       />
