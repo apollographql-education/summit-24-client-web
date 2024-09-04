706cbfba4a37ebd4b1d17c42f0af7288a46062ed
Jerel Miller <jerelmiller@gmail.com>
2024-09-03 23:09:18 -0600

    Add type policy for search results

--- src/apolloClient.tsx
+++ src/apolloClient.tsx
@@ -17,6 +17,61 @@ const cache = new InMemoryCache({
             "criteria",
             ["checkInDate", "checkOutDate", "sortBy", "numOfBeds"],
           ],
+          read: (allListings, { args, storage }) => {
+            if (!allListings) {
+              return;
+            }
+
+            const page = args?.criteria?.page || 1;
+            const limit = args?.criteria?.limit || 5;
+
+            const offset = (page - 1) * limit;
+
+            const listings = allListings
+              ? allListings.slice(offset, offset + limit)
+              : [];
+
+            if (listings.length > 0 && listings.every(Boolean)) {
+              return listings;
+            }
+
+            if (storage.fetchedPages?.get(limit)?.has(page)) {
+              return [];
+            }
+
+            return undefined;
+          },
+          merge: (existing, incoming = [], { args, storage }) => {
+            const page = args?.criteria?.page || 1;
+            const limit = args?.criteria?.limit || 5;
+            const offset = (page - 1) * limit;
+
+            storage.fetchedPages ||= new Map<number, Set<number>>();
+            let set = storage.fetchedPages.get(limit);
+
+            if (!set) {
+              set = new Set<number>();
+              storage.fetchedPages.set(limit, set);
+            }
+
+            set.add(page);
+
+            if (incoming.length === 0) {
+              return existing;
+            }
+
+            const listings = existing
+              ? existing.slice(0)
+              : Array(offset + limit).fill(undefined);
+
+            for (let i = 0; i < limit; i++) {
+              if (incoming[i]) {
+                listings[i + offset] = incoming[i];
+              }
+            }
+
+            return listings;
+          },
         },
         listing: {
           read: (_, { args, toReference }) => {
