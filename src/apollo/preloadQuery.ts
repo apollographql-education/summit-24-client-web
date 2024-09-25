1e2192e9a5b545324ee4ba03c418ff194438cb54
Jerel Miller <jerelmiller@gmail.com>
2024-09-24 18:31:44 -0600

    Comment out query preloader

--- src/apollo/preloadQuery.ts
+++ src/apollo/preloadQuery.ts
@@ -1,4 +1,2 @@
-import { createQueryPreloader } from "@apollo/client";
-import { client } from "./client";
-
-export const preloadQuery = createQueryPreloader(client);
+// import { createQueryPreloader } from "@apollo/client";
+// import { client } from "./client";
