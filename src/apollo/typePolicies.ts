f13a54175766fb41f9c1dbeb3ccbb8be8f7305a0
Jerel Miller <jerelmiller@gmail.com>
2024-10-02 22:40:03 -0600

    Add exercise 2 setup

--- src/apollo/typePolicies.ts
+++ src/apollo/typePolicies.ts
@@ -1,5 +1,11 @@
 import { TypePolicies } from "@apollo/client";
 
+/* Exercise 2
+ *
+ * Docs on keyFields:
+ * https://www.apollographql.com/docs/react/caching/cache-configuration#customizing-cache-ids
+ *
+ */
 export const typePolicies: TypePolicies = {
   // Listing: {
   //   keyFields: [],
--- src/components/InflationButton.tsx
+++ src/components/InflationButton.tsx
@@ -1,11 +1,19 @@
 import { gql, useApolloClient } from "@apollo/client";
 import { Button } from "@chakra-ui/react";
 import { GetFeaturedListingsFromCacheQuery } from "./__generated__/InflationButton.types";
+// Use this type to provide type safety for cache.modify
+// import type { Listing } from "../__generated__/types";
 
-/* Exercise 1
+/* Exercise 2
  *
  * Docs on cache.readQuery:
  * https://www.apollographql.com/docs/react/caching/cache-interaction#readquery
+ *
+ * Docs on cache.identify:
+ * https://www.apollographql.com/docs/react/caching/cache-interaction#obtaining-an-objects-cache-id
+ *
+ * Docs on cache.modify:
+ * https://www.apollographql.com/docs/react/caching/cache-interaction#using-cachemodify
  */
 
 export function InflationButton() {
@@ -28,7 +36,25 @@ export function InflationButton() {
           `,
         });
 
-        console.log(data);
+        // Follow-along: object identification
+        // if (data) {
+        //   const listing = data.featuredListings[0];
+        //   const id = cache.identify();
+        //   console.log("cacheId", id);
+        // }
+
+        // Follow-along: cache modification
+        // cache.modify({})
+
+        // Exercise:
+        // Modify each featuredListings's costPerNight when this
+        // button is cliecked. Use the code below as a starter.
+        //
+        // if (data) {
+        //   data.featuredListings.forEach((listing) => {
+        //     // TODO: Implement cache modification here for exercise 2
+        //   });
+        // }
       }}
     >
       $$$ Inflate costs $$$
